81713bdb78ada753431054c0eab8fda0e36684b1
Borombo <e.rombo@yahoo.com>
2016-02-12 23:54:22 +0100

    Gestion des Emojis + Excel Valide

--- app/src/main/java/com/example/phantom/lvmessage_1/MainActivity.java
+++ app/src/main/java/com/example/phantom/lvmessage_1/MainActivity.java
@@ -4,11 +4,9 @@ import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -48,14 +46,29 @@ public class MainActivity extends AppCompatActivity {
         HSSFSheet sheet = classeur.getSheetAt(0);
         Iterator i = sheet.rowIterator();
         boolean found = false;
+        String emoji;
         i.next();
         while(i.hasNext() && !found){
             HSSFRow tmp = (HSSFRow) i.next();
             if(isToday(now,tmp.getCell(1).toString())){
                 found = true;
                 this.message.setText(tmp.getCell(2).toString());
+                HSSFCell emo = tmp.getCell(3);
+                if (emo != null){
+                    emoji = getEmoji(tmp.getCell(3).toString());
+                    this.message.append("\n" + emoji);
+                }
             }
         }
+    }
+
+    public String getEmoji(String unicode){
+        String res = "";
+        String[] tmp = unicode.split("/");
+        for (String s: tmp) {
+            res += new String(Character.toChars(Integer.decode(s))) + " ";
+        }
+        return res;
     }
 
     public boolean isToday(Calendar now, String date){
@@ -63,8 +76,6 @@ public class MainActivity extends AppCompatActivity {
         String[] tmp = date.split("/");
         int day = now.get(Calendar.DAY_OF_MONTH);
         int month = now.get(Calendar.MONTH) + 1;
-        Log.d("Day_Tab",tmp[0]);
-        Log.d("Month_Tab", tmp[1]);
         int dayTab = Integer.valueOf(tmp[0]);
         int monthTab = Integer.valueOf(tmp[1]);
         if ((dayTab == day) && (monthTab == month)){
@@ -84,27 +95,5 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
