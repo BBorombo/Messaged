b2faec5f96772d1212b7555e0e24fd8a74e05b39
Borombo <e.rombo@yahoo.com>
2016-02-12 22:41:01 +0100

    Lecture Message Fichier Excel : Ok -> Modification Format Date Fichier Excel

--- app/src/main/java/com/example/phantom/lvmessage_1/MainActivity.java
+++ app/src/main/java/com/example/phantom/lvmessage_1/MainActivity.java
@@ -4,21 +4,26 @@ import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Calendar;
+import java.util.Iterator;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private HSSFWorkbook classeur;
-
+    private TextView message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,20 +31,53 @@ public class MainActivity extends AppCompatActivity {
 
         //Récupération des champs, ajout de la police
         TextView day_msg = (TextView) findViewById(R.id.day_msg);
-        TextView message = (TextView) findViewById(R.id.message);
+        message = (TextView) findViewById(R.id.message);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "Lobster 1.4.otf");
 
         day_msg.setTypeface(font);
         message.setTypeface(font);
 
+        openExcelFIle();
+        setMessage();
+
         }
+
+    public void setMessage(){
+        Calendar now = Calendar.getInstance();
+        HSSFSheet sheet = classeur.getSheetAt(0);
+        Iterator i = sheet.rowIterator();
+        boolean found = false;
+        i.next();
+        while(i.hasNext() && !found){
+            HSSFRow tmp = (HSSFRow) i.next();
+            if(isToday(now,tmp.getCell(1).toString())){
+                found = true;
+                this.message.setText(tmp.getCell(2).toString());
+            }
+        }
+    }
+
+    public boolean isToday(Calendar now, String date){
+        boolean res = false;
+        String[] tmp = date.split("/");
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int month = now.get(Calendar.MONTH) + 1;
+        Log.d("Day_Tab",tmp[0]);
+        Log.d("Month_Tab", tmp[1]);
+        int dayTab = Integer.valueOf(tmp[0]);
+        int monthTab = Integer.valueOf(tmp[1]);
+        if ((dayTab == day) && (monthTab == month)){
+            res = true;
+        }
+        return res;
+    }
 
     public void openExcelFIle(){
 
         final AssetManager assetManager = getAssets();
         try {
-            InputStream input = assetManager.open("Messages_Appli.xlsx");
+            InputStream input = assetManager.open("Messages_Appli.xls");
             POIFSFileSystem myFileSystem = new POIFSFileSystem(input);
             this.classeur = new HSSFWorkbook(myFileSystem);
         }catch (IOException e) {
